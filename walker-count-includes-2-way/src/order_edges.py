03b85e506ad9360a0f7b9be0922b09f5cc4c9f7b
Gabriel U <ullmanng162@gmail.com>
2022-08-29 14:22:27 -0400

    add new vector generation format

--- walker-count-includes-2-way/src/order_edges.py
+++ walker-count-includes-2-way/src/order_edges.py
@@ -5,27 +5,63 @@ def get_vector_format(line):
     result = line.strip().replace("\"", "").split("->")
     return list(map(lambda item: item.strip(), result)) 
 
-def generate_unique_node_ids(node_list):
-    ascii_begin = 65
-    ascii_end = 91
-    ascii_limit = ascii_end - ascii_begin
-    alphabet = np.arange(ascii_begin, ascii_end)
+def generate_unique_node_ids(node_list, id_type):
+    name_map = {}
     all_node_names = np.array([])
+
+    if id_type == "string":
+        ascii_begin = 65
+        ascii_end = 91
+        ascii_limit = ascii_end - ascii_begin
+        alphabet = np.arange(ascii_begin, ascii_end)
+    elif id_type == "number":
+        starting_number = 1
+
+    #get unique names
     for node in node_list:
         all_node_names = np.append(all_node_names, str(node).strip())
     unique_names = np.unique(all_node_names)
-    name_map = {}
+
     counter = 0
-    cycle = 1
-    for name in unique_names:
-        name_map[name] = chr(alphabet[counter]) + str(cycle)
-        counter += 1
-        if counter == ascii_limit:
-            counter = 0
-            cycle += 1
+    if id_type == "string":
+        cycle = 1
+        for name in unique_names:
+            name_map[name] = chr(alphabet[counter]) + str(cycle)
+            counter += 1
+            if counter == ascii_limit:
+                counter = 0
+                cycle += 1
+    elif id_type == "number":
+        counter = starting_number
+        for name in unique_names:
+            name_map[name] = str(counter)
+            counter += 1
+    else:
+        print("Error: unknown ID type. Types allowed: string, number.")
+
     return name_map
 
-def gen_vector(save_to_file=True):
+def concat_vector_items(id_type, ordered_dot_ids):
+    result_vector = ""
+    first_item = True
+    if id_type == "string":
+        for file_id in ordered_dot_ids:
+            if first_item:
+                result_vector += "inc".join(file_id)
+                first_item = False
+            else:
+                result_vector += "," + ("inc".join(file_id))
+    elif id_type == "number":
+        for file_id in ordered_dot_ids:
+            pair = ",".join(file_id)
+            if first_item:
+                result_vector += "(" + pair + ",1)"
+                first_item = False
+            else:
+                result_vector += ",(" + pair + ",1)"
+    return result_vector
+
+def gen_vector(id_type="string", save_to_file=True):
     # order and save
     ds = pd.read_csv('edge_count.csv')
     ds = ds.sort_values(by="sum", ascending=False)
@@ -33,13 +69,12 @@ def gen_vector(save_to_file=True):
     ds.to_csv('edges_ordered.csv')
 
     # use the ordered ds
-    result_vector = ""
     dot_file = open("subsystem.dot", "r")
     dot_file_lines = []
     for line in dot_file:
         dot_file_lines.append(line)
     ordered_file_names = ds['edge'].values #[0:2]
-    name_map = generate_unique_node_ids(ordered_file_names)
+    name_map = generate_unique_node_ids(ordered_file_names, id_type)
     ordered_dot = []
     for file_name in ordered_file_names:
         for line in dot_file_lines:
@@ -57,13 +92,7 @@ def gen_vector(save_to_file=True):
             pass
         ordered_dot_ids.append([node, include])
 
-    first_item = True
-    for file_id in ordered_dot_ids:
-        if first_item:
-            result_vector += "inc".join(file_id)
-            first_item = False
-        else:
-            result_vector += "," + ("inc".join(file_id))
+    result_vector = concat_vector_items(id_type, ordered_dot_ids)
 
     if not save_to_file:
         print(result_vector)
@@ -77,4 +106,4 @@ def gen_vector(save_to_file=True):
         map_file.write(str(name_map))
         map_file.close()
 
-gen_vector()
+gen_vector("number")
